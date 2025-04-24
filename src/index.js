74b584a2181a820de0b148aefcdaa4fd70cf129a
Dmytro Lobanov <dspwww@gmail.com>
2025-04-24 23:27:21 +0100

    feat: Add webpack and reconfigurate dev env.
    - Config webpack deelopment proccess
    - Use .env to hide API_KEY

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import './style.css';
 import { getWeatherByCity } from "./weatherAPI.js";
 
 let prevCity;
--- src/weatherAPI.js
+++ src/weatherAPI.js
@@ -1,7 +1,5 @@
-const API_KEY = 'U9Z3YZHD2NKPCDLNMS3BFQHNL';
-
 function getURL(city) {
-  return `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=us&elements=name%2Caddress%2Ctemp%2Cfeelslike%2Ccloudcover%2Cconditions%2Cdescription%2Cicon&include=current&key=${API_KEY}&contentType=json`;
+  return `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=us&elements=name%2Caddress%2Ctemp%2Cfeelslike%2Ccloudcover%2Cconditions%2Cdescription%2Cicon&include=current&key=${process.env.API_KEY}&contentType=json`;
 }
 
 export function getWeatherByCity(city) {
